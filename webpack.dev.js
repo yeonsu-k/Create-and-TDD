7de767ac31af205c04e831c6af4b842e33e6c566
dustnzlzl <dustnzlzl@naver.com>
2023-08-05 18:28:48 +0900

    Update file name webpack.config.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,9 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const config = require("./webpack.config.js");
 
-module.exports = merge(common, {
-  mode: 'development', // 현재 개발 모드
-  devtool: 'eval', // 최대성능, 개발환경에 추천
+module.exports = merge(config, {
+  mode: "development", // 현재 개발 모드
+  devtool: "eval", // 최대성능, 개발환경에 추천
   devServer: {
     historyApiFallback: true,
     port: 3000,

a9e8d96187f678d99a9e787547a50a4e45a47879
dustnzlzl <dustnzlzl@naver.com>
2023-08-06 00:08:34 +0900

    Add Webpack and tsconfig settings to the storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,9 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
+import custom from "../webpack.config.js";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(ts|tsx|mdx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -13,7 +15,20 @@ const config: StorybookConfig = {
     options: {},
   },
   docs: {
+    // 모든 컴포 파일에 docs파일 만들기
     autodocs: true,
+  },
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.plugins = [
+        ...(config.resolve.plugins || []),
+        new TsconfigPathsPlugin({ extensions: config.resolve.extensions }),
+      ];
+    }
+    return {
+      ...config,
+      module: { ...config.module, rules: [...(config.module as any).rules, ...custom.module.rules] },
+    };
   },
 };
 export default config;

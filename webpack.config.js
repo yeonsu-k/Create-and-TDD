3b8de0a1381881de9a1cddbae112f110b505519e
dustnzlzl <dustnzlzl@naver.com>
2023-08-05 21:55:22 +0900

    webpack image, font loader 추가

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,15 @@ module.exports = {
         use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
-        test: /\.(png|jpe?g|gif)$/,
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           {
             loader: "file-loader",

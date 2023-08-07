b7e1ad812d617a3d40c8fe3d2b0991217ca88c02
dustnzlzl <dustnzlzl@naver.com>
2023-08-08 01:21:03 +0900

    Setting Jest and react-testing-library

---
+++ jest.config.ts
@@ -0,0 +1,34 @@
+import { type JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: "ts-jest", // .ts와 .tsx을 CommonJS로 변환
+  testEnvironment: "jsdom", // 테스트 환경으로 jsdom을 사용
+  testMatch: ["**/__tests__/**/*.test.(ts|tsx)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.jest.json",
+        babel: true,
+      },
+    ],
+  },
+  modulePaths: ["<rootDir>"],
+  setupFilesAfterEnv: ["<rootDir>/setupTest.ts"], // 각각의 테스트 전에 실행할 모듈을 경로(setupTest를 이용하여 환경 설정)
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  moduleNameMapper: {
+    "\\.(css|scss)$": "identity-obj-proxy",
+    "^src/(.*)$": "<rootDir>/src/$1",
+    "^assets/(.*)$": "<rootDir>/src/assets/$1",
+    "^components/(.*)$": "<rootDir>/src/components/$1",
+    "^features/(.*)$": "<rootDir>/src/features/$1",
+    "^hooks/(.*)$": "<rootDir>/src/hooks/$1",
+    "^pages/(.*)$": "<rootDir>/src/pages/$1",
+    "^services/(.*)$": "<rootDir>/src/services/$1",
+    "^store/(.*)$": "<rootDir>/src/store/$1",
+    "^styles/(.*)$": "<rootDir>/src/styles/$1",
+    "^stories/(.*)$": "<rootDir>/src/stories/$1",
+  },
+};
+
+export default jestConfig;

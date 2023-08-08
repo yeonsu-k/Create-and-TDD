06bf545bb4e3970a35747f441bf3fb1d2162726b
dustnzlzl <dustnzlzl@naver.com>
2023-08-09 00:21:12 +0900

    Modify router main path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
 import MainPage from "pages/MainPage";
 import LoginPage from "pages/LoginPage";
 import SignupPage from "pages/SignupPage";
@@ -7,12 +7,13 @@ import SignupPage from "pages/SignupPage";
 const App = () => (
   <>
     <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-      <Link to="/">Main</Link>
+      <Link to="/home">Main</Link>
       <Link to="/login">Login</Link>
       <Link to="/signup">Signup</Link>
     </div>
     <Routes>
-      <Route path="/" element={<MainPage />} />
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<MainPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
     </Routes>

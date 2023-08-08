b13ab51010a0ae1bab17076165d8105f2c564043
dustnzlzl <dustnzlzl@naver.com>
2023-08-09 00:13:29 +0900

    Updatae webpack development mode devtool

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,7 @@ const config = require("./webpack.config.js");
 
 module.exports = merge(config, {
   mode: "development", // 현재 개발 모드
-  devtool: "eval", // 최대성능, 개발환경에 추천
+  devtool: "eval-source-map", // 최대성능, 개발환경에 추천
   devServer: {
     historyApiFallback: true,
     port: 3000,

3dd0deeacdd8447ce72b94c1989393b6afc97bef
dustnzlzl <dustnzlzl@naver.com>
2023-08-10 23:12:45 +0900

    Make the same setting

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -5,9 +5,7 @@ module.exports = merge(config, {
   mode: "development", // 현재 개발 모드
   devtool: "eval-source-map", // 최대성능, 개발환경에 추천
   output: {
-    path: path.resolve(__dirname, "build"),
     publicPath: "/",
-    filename: "bundle.js",
   },
   devServer: {
     historyApiFallback: true,

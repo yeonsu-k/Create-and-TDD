9edd0654248804008bc60eed3f1724bb3fd56d74
dustnzlzl <dustnzlzl@naver.com>
2023-08-17 01:11:08 +0900

    다크모드 적용하기

---
+++ src/components/darkmodeToggle.tsx
@@ -0,0 +1,75 @@
+import React, { ReactElement, useContext } from "react";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import styled from "@emotion/styled";
+import { ThemeContext } from "src/App";
+import { lightTheme, Theme } from "@styles/theme";
+import { MEDIA_QUERY_END_POINT } from "@constants/.";
+
+interface ToggleProps {
+  theme: Theme;
+}
+
+export default function DarkModeToggle(): ReactElement {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <ToggleButton onClick={toggleTheme} theme={theme}>
+      {theme === lightTheme ? (
+        <>
+          <Emoji>
+            <DarkModeIcon aria-label="darkMoon" />
+          </Emoji>
+          <ModeContent>다크 모드</ModeContent>
+        </>
+      ) : (
+        <>
+          <Emoji>
+            <LightModeIcon aria-label="lightSun" />
+          </Emoji>
+          <ModeContent>라이트 모드</ModeContent>
+        </>
+      )}
+    </ToggleButton>
+  );
+}
+
+const ToggleButton = styled("button")<ToggleProps>`
+  position: fixed;
+  width: 115px;
+  height: 45px;
+  right: 1.5rem;
+  bottom: 1.5rem;
+  border-radius: 30px;
+  padding-left: 2px;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  background: ${({ theme }) => theme.TOGGLE_BACKGROUND};
+  color: ${({ theme }) => theme.MAIN_FONT};
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+
+  &:hover {
+    filter: brightness(${({ theme }) => (theme === lightTheme ? "0.9" : "1.13")});
+  }
+  @media (max-width: ${MEDIA_QUERY_END_POINT.TABLET}) {
+    display: none;
+  }
+`;
+
+const Emoji = styled.figure`
+  width: 33px;
+  height: 33px;
+  border-radius: 100%;
+  font-size: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModeContent = styled.p`
+  font-size: 0.8rem;
+  margin-left: 5px;
+`;
---
+++ src/constants/index.ts
@@ -0,0 +1,32 @@
+export const MEDIA_QUERY_END_POINT = {
+  SMALL: "470px",
+  MOBILE: "768px",
+  TABLET: "1024px",
+  DESKTOP: "1200px",
+  LARGE: "1440px",
+  XLARGE: "1919px",
+};
+
+export const PALLETS_LIGHT = {
+  MAIN: "#6868AD",
+  SUB: "#e8e8fa",
+  BACKGROUND: "#f8f9fa",
+  SUBBACKGROUND: "#f0f2f5",
+  CARD_BACKGROUND: "#fff",
+  MAIN_FONT: "#202124", //rgb(52, 58, 64)
+  SUB_FONT: "#495057",
+  POINT_FONT: "#868E96",
+  ICON: "#868E96",
+  BORDER: "#adb6bd",
+};
+
+export const PALLETS_DARK = {
+  MAIN: "#6868AD",
+  SUB: "#e8e8fa",
+  BACKGROUND: "#202124",
+  CARD_BACKGROUND: "#1e1e1e",
+  MAIN_FONT: "rgba(255, 255, 255, 0.87)",
+  SUB_FONT: "rgba(255, 255, 255, 0.6)",
+  ICON: "#868E96",
+  BORDER: "#dae4ed", //수정 될 가능성 높음
+};

42678f0ddcb06c69fe8b67a1c6c7e5c44cff757b
dustnzlzl <dustnzlzl@naver.com>
2023-08-22 01:22:18 +0900

    Update config.ts - paths setting

--- jest.config.ts
+++ jest.config.ts
@@ -22,8 +22,6 @@ const jestConfig: JestConfigWithTsJest = {
     "^@hooks/(.*)$": "<rootDir>/src/hooks/$1",
     "^@assets/(.*)$": "<rootDir>/src/assets/$1",
     "^@components/(.*)$": "<rootDir>/src/components/$1",
-    "^@constants/(.*)$": "<rootDir>/src/constants/$1",
-    "^@features/(.*)$": "<rootDir>/src/features/$1",
     "^@pages/(.*)$": "<rootDir>/src/pages/$1",
     "^@store/(.*)$": "<rootDir>/src/store/$1",
     "^@styles/(.*)$": "<rootDir>/src/styles/$1",
